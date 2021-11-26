b538c55c8ea5f2082bed03caa94c0b7715a525a9
antonio attadia <antonioatt@yahoo.it>
2021-11-26 11:08:12 +0000

    js

--- netlify-functions/hello.js
+++ netlify-functions/hello.js
@@ -1,7 +1,8 @@
 exports.handler = async function(event, context) {
+    // 'content-type': "application/json"
     return {
         statusCode: 200,
         body: JSON.stringify({message: "Hello World"}),
-        'content-type': "application/json"
+       
     };
 }
